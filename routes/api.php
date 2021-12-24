7ff91ed579ac339949e2410343ee04d4797a1918
andrewearls <andrew.e.earls@gmail.com>
2021-12-23 22:25:09 -0800

    sanctum token generation

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ use App\Http\Controllers;
 
 Route::post('/registration', [Controllers\Auth\RegisterController::class, 'register']);
 
+Route::post('/sanctum/token', [Controllers\Api\Sanctum\TokenController::class, 'create']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
