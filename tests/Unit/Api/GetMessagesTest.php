1f82146b47c357489935bd32bf1ebacae593491b
andrewearls <andrew.e.earls@gmail.com>
2022-01-15 11:32:31 -0800

    Messages tests

---
+++ tests/Unit/Api/GetMessagesTest.php
@@ -0,0 +1,12 @@
+<?php
+
+
+// before each
+    //create a user
+    //create a message
+// using sanctum
+    //get the message
+
+it('can get messages', function () {
+    expect(true)->toBeTrue();
+});
