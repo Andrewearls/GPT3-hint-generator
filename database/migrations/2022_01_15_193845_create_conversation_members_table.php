f839cf3bdd81711e4ae9e4e5ed34c9f65b7ef52d
andrewearls <andrew.e.earls@gmail.com>
2022-01-22 18:43:15 -0800

    Conversations members table migration

---
+++ database/migrations/2022_01_15_193845_create_conversation_members_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConversationMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversation_members', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('read_status')->default(false);
+            $table->integer('conversation_id');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversation_members');
+    }
+}
